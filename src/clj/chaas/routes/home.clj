d069b4bad977154e00046f42c0286534ae619717
Salvatore Tosti <saltosti@gmail.com>
2021-09-01 15:29:47 -0400

    Add hover link behavior

--- src/clj/chaas/routes/home.clj
+++ src/clj/chaas/routes/home.clj
@@ -32,9 +32,9 @@
   [:nav {:class "relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-500 mb-3"}
    [:div {:class "container px-4 mx-auto flex flex-wrap items-center justify-between"}
     [:div {:class "relative flex justify-between w-auto px-4 static block justify-start"}
-     [:a {:class "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" :href "#"}
+     [:a {:class "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white hover:opacity-75" :href "#"}
       "Home"]
-     [:a {:class "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" :href "/api/api-docs/"}
+     [:a {:class "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white hover:opacity-75" :href "/api/api-docs/"}
       "API"]]
     [:div {:class "flex flex-grow items-center"}
      [:ul {:class "flex flex-row list-none ml-auto"}
